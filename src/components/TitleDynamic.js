18709b49151c65e27e62708eec1687dc2de7cf5a
Luis Felipe Ramalho Oliveira <50757892+luisfeliperamalho@users.noreply.github.com>
2023-10-04 17:30:25 -0300

    first commit

---
+++ src/components/TitleDynamic.js
@@ -0,0 +1,23 @@
+class TitleDynamic extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: "open" });
+
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = "Felipe";
+
+        const style = document.createElement("style");
+        style.textContent = `
+            h1 {
+                color: red;
+            }
+        `;
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+
+    }
+}
+
+customElements.define('title-dynamic', TitleDynamic);
