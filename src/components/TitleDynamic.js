7d61eb03342f497bf4f3e827de026c6bf9e96363
Luis Felipe Ramalho <luisfeliperamalho@outlook.com>
2023-10-05 21:02:19 -0300

    ajustes finais

--- src/components/TitleDynamic.js
+++ src/components/TitleDynamic.js
@@ -1,23 +1,25 @@
 class TitleDynamic extends HTMLElement {
     constructor() {
-        super();
-
-        const shadow = this.attachShadow({ mode: "open" });
-
-        const componentRoot = document.createElement("h1");
-        componentRoot.textContent = "Felipe";
-
-        const style = document.createElement("style");
-        style.textContent = `
-            h1 {
-                color: red;
-            }
-        `;
-
-        shadow.appendChild(componentRoot);
-        shadow.appendChild(style);
-
+      super();
+  
+      const shadow = this.attachShadow({ mode: "open" });
+  
+      //base do component <h1>Luis Felipe</h1>
+      const componentRoot = document.createElement("h1");
+      componentRoot.textContent = this.getAttribute("title");
+  
+      //estilizar o component
+      const style = document.createElement("style");
+      style.textContent = `
+        h1{
+          color: red;
+        }
+      `;
+  
+      //enviar para a shadow
+      shadow.appendChild(componentRoot);
+      shadow.appendChild(style);
     }
-}
-
-customElements.define('title-dynamic', TitleDynamic);
+  }
+  
+  customElements.define("title-dynamic", TitleDynamic);
